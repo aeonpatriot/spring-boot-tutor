2292511f57550f8ea140a037a4c357f526685264
aeonpatriot <has8ultimate@gmail.com>
2025-07-21 14:44:40 +0800

    Initial commit - Spring Boot project

---
+++ src/main/java/com/example/spring_boot_tutor/SpringBootTutorApplication.java
@@ -0,0 +1,13 @@
+package com.example.spring_boot_tutor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootTutorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootTutorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/spring_boot_tutor/SpringBootTutorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.spring_boot_tutor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootTutorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
